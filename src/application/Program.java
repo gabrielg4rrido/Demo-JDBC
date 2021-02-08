7d5980f8bc82800786003f2438d6bfd9493dd913
Gabriel Garrido <gabrielgarridoag@dcc.ufrj.br>
2021-02-08 17:59:54 -0300

    Criação da classe Department.

---
+++ src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import models.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Department d1 = new Department(1, "Computers");
+		
+		System.out.println(d1);
+	}
+
+}
